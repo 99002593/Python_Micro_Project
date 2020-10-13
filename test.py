9695c2795d2a636d8295241f9c84a07c4a75a136
99002593 <71540777+99002593@users.noreply.github.com>
2020-10-13 18:56:25 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+from extraction import Resume,CSV
+
+class Recruitmenttest(unittest.TestCase):
+	  def setUp(self): 
+	  	pass;
+	  def test_candidate_present(self):
+	  	self.assertEqual('shekarjk.mec@gmail.com',resumegmail());
+        
+
+if __name__ == '__main__':
+    unittest.main() 
+
