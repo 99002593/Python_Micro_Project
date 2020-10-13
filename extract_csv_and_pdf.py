7b2601883c9e71ed43d6ca93cf68827bf4d3d782
99002593 <71540777+99002593@users.noreply.github.com>
2020-10-13 16:47:07 +0530

    Update extract_csv_and_pdf.py

--- extract_csv_and_pdf.py
+++ extract_csv_and_pdf.py
@@ -7,16 +7,6 @@ import pandas as pd
 
 files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.pdf')]
 
-###############################################################################################extract gmail from resume
-
-all_mail=[]
-for i in files:
-	text = textract.process(i)
-	res=str(text)
-	result=re.findall('[\w+][\w+\.]+@[\w+][\w+\.]+[a-z A-Z 0-9]',res)                       #regular expression for extracting gmail
-	all_mail.extend(result)
-#print(result)
-print("all_mail:",all_mail)     								   #store all the emails in a list
 
 ################################################################################################From csv file extract all gmails
 col_list = ["Email"]
@@ -33,7 +23,7 @@ message="""Dear Candidate,
 			We are glad to inform that you are selected for the online interview process. Kindly respond to this with your CV's within the next 24 hours.
  
 			Regards
-			Torres"""
+			Dr.F.Torres"""
 for dest in all_mail_csv:
 	try:
 	   conn=smtplib.SMTP('smtp.gmail.com',587)
