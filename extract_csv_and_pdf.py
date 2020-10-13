d303ae4a0a5806c613b6075935367e4f9fecc940
99002565 <71690598+99002565@users.noreply.github.com>
2020-10-13 16:34:41 +0530

    Update extract_csv_and_pdf.py

--- extract_csv_and_pdf.py
+++ extract_csv_and_pdf.py
@@ -23,4 +23,27 @@ col_list = ["Email"]
 df = pd.read_csv("data.csv", usecols=col_list)
 all_mail_csv=list(df["Email"])
 print("all_mail_csv:",all_mail_csv)############################################################store all the emails from csv in a list
-
+###############################################################################################Sending automated mails 
+sender="pythonprojsns@gmail.com"
+password="veertanu"
+message="""Dear Candidate,
+ 
+			Greetings from PQB,
+ 
+			We are glad to inform that you are selected for the online interview process. Kindly respond to this with your CV's within the next 24 hours.
+ 
+			Regards
+			Torres"""
+for dest in all_mail_csv:
+	try:
+	   conn=smtplib.SMTP('smtp.gmail.com',587)
+	   conn.starttls()
+	   conn.ehlo()
+	   conn.login(sender,password)
+	   conn.ehlo()
+	   conn.sendmail(sender, dest, message)         
+	   print("Successfully sent email")
+	except smtplib.SMTPException:
+	   print("Error: unable to send email")
+	finally:
+		conn.quit()
